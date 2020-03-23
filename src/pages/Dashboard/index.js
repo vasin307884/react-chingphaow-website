79b8ba86ff559a12ad87a8d9095b494de8089e1d
Vasin <6031305033@lamduan.mfu.ac.th>
2020-03-23 15:07:17 +0700

    function commit

---
+++ src/pages/Dashboard/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default () => (
+  <div className="has-text-centered">
+    <section class="hero is-dark">
+      <div className="container">
+        <h1 className="title">Dashboard Page</h1>
+      </div>
+    </section>
+  </div>
+)
---
+++ src/pages/Request/index.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react'
+export default class Request extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      requestsdata: [],
+      loading: false,
+      error: null,
+    }
+  }
+  fetchRequestData() {
+    fetch('https://chingphaow-application.herokuapp.com/requests/')
+      .then((response) => response.json())
+      .then((response) => {
+        this.setState({
+          isLoading: false,
+          requestsdata: response.data,
+          refreshing: false
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+  componentDidMount(){
+    this.fetchRequestData();
+  }
+  render() {
+    return (
+      <table>
+      <thead>
+        <tr>
+          <th>ไอดี</th>
+          <th>ชื่อผู้ส่ง</th>
+          <th>ที่อยู่</th>
+          <th>เบอร์โทร</th>
+          <th>ละติจูด</th>
+          <th>ลองจิจูด</th>
+          <th>วันที่ส่งมา</th>
+          <th>วันที่เริ่ม</th>
+          <th>สถานะ</th>
+          <th>อัพเดทล่าสุดเมื่อ</th>
+        </tr>
+      </thead>
+      {this.state.requestsdata.map(requestsdata=>(
+      <tbody>
+        <tr>
+          <td>{requestsdata.id}</td>
+          <td>{requestsdata.name}</td>
+          <td>{requestsdata.address}</td>
+          <td>{requestsdata.phone}</td>
+          <td>{requestsdata.latitude}</td>
+          <td>{requestsdata.longitude}</td>
+          <td>{requestsdata.fromdate}</td>
+          <td>{requestsdata.todate}</td>
+          <td>{requestsdata.statusValue}</td>
+          <td>{requestsdata.lastupdate}</td>
+        </tr>
+      </tbody>
+      ))}
+    </table>
+
+    );
+  }
+}
+
