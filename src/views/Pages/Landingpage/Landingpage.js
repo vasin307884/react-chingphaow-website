c839c7324d5ba8887fbe34b3b15ed204e7b7cfd7
Ratchanon Noknoi <film_phone@hotmail.com>
2020-04-04 17:58:51 +0700

    edit Landingpage.js

--- src/views/Pages/Landingpage/Landingpage.js
+++ src/views/Pages/Landingpage/Landingpage.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { Button, Card, CardBody, CardGroup, Col, Container, Form, Input, InputGroup, InputGroupAddon, InputGroupText, Row } from 'reactstrap';
+import { Badge, Card, CardBody, CardFooter, CardHeader, Col, Row, Collapse, Fade } from 'reactstrap';
+import { AppSwitch } from '@coreui/react'
 import { Map, GoogleApiWrapper, Marker, InfoWindow } from 'google-maps-react';
 import './App.css';
 class Landingpage extends Component {
@@ -61,8 +62,7 @@ class Landingpage extends Component {
       
   render() {
     return (
-        
-<div class="w3-top">
+      <div >
   <div class="w3-bar w3-white w3-card" id="myNavbar">
     <a href="#home" class="w3-bar-item w3-button w3-wide">LOGO</a>
     
@@ -70,12 +70,12 @@ class Landingpage extends Component {
       <a href="#about" class="w3-bar-item w3-button">ABOUT</a>
       <a href="#team" class="w3-bar-item w3-button"><i class="fa fa-user"></i> TEAM</a>
       <a href="#work" class="w3-bar-item w3-button"><i class="fa fa-th"></i> WORK</a>
-      <a href="#pricing" class="w3-bar-item w3-button"><i class="fa fa-usd"></i> PRICING</a>
       <a href="#contact" class="w3-bar-item w3-button"><i class="fa fa-envelope"></i> CONTACT</a>
+      <Link to="/login"><a href="#contact" class="w3-bar-item w3-button"> LOGIN</a></Link>
     </div>
     </div>
     
-      {/* <div className="app flex-row align-items-center">
+      <div className="app flex-row align-items-center" id="homepage">
           
          <Map
         google={this.props.google}
@@ -126,7 +126,7 @@ class Landingpage extends Component {
         </InfoWindow>
       </Map>
 
-      </div> */}
+      </div>
      
      {/* <header class="w3-container w3-red w3-center" >
      <h1 class="w3-margin w3-jumbo">START PAGE</h1>
@@ -136,16 +136,30 @@ class Landingpage extends Component {
      <div class="w3-row-padding w3-padding-64 w3-container">
   <div class="w3-content">
     <div class="w3-twothird">
-      <h1>Lorem Ipsum</h1>
-      <h5 class="w3-padding-32">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</h5>
+      <h1>ชิงเผาคืออะไร</h1>
+      <h5 class="w3-padding-32">การชิงเผาเป็นวิธีการหนึ่งของการเผาตามกำหนด (Prescribe Burning)
+            อันเป็นการใช้ประโยชน์จากไฟเพื่อการจัดการป่าไม้
+            การชิงเผามีวัตถุประสงค์หลัก เพื่อลดปริมาณเชื้อเพลิงในป่าลง
+            ทั้งนี้เพื่อเป็นการลดโอกาสในการเกิดไฟป่า หรือถ้าเกิดไฟป่าขึ้น
+            ความรุนแรงแลอันตรายของไฟนั้น (Fire Hazard) จะมีน้อยลง
+            สามารถควบคุมไฟได้ง่ายและปลอดภัย
+</h5>
 
-      <p class="w3-text-grey">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-        laboris nisi ut aliquip ex ea commodo consequat.</p>
+      <p class="w3-text-grey">สำหรับประเทศไทย มีการชิงเผาเพื่อป้องกันไฟในสวนป่ามาเป็นเวลานานแล้ว
+            โดยเฉพาะอย่างยิ่งในระยะที่ต้นไม้ยังมีขนาดเล็ก
+            ซึ่งจะมีเชื้อเพลิงจำพวกวัชพืชต่างๆ ขึ้นอยู่ในปริมาณมาก
+            สำหรับในป่าธรรมชาติ
+            หลังจากที่กรมป่าไม้ได้ดำเนินการควบคุมไฟป่าในพื้นที่ป่าทั่วประเทศ
+            ทำให้ในหลายพื้นที่มีการสะสมของเชื้อเพลิงในปริมาณมาก
+            และในปีใดที่เกิดไฟไหม้ขึ้นในพื้นที่นั้น ไฟจะมีความรุนแรงมาก
+            จนยากต่อการควบคุมและเป็นอันตรายอย่างยิ่งต่อพนักงานดับไฟป่า ดังนั้น
+            การชิงเผาจึงเริ่มเข้ามามีบทบาทสำคัญในการควบคุมไฟในป่าธรรมชาติมากขึ้นเรื่อยๆ
+            เช่นกัน
+</p>
     </div>
 
     <div class="w3-third w3-center">
-      <i class="fa fa-anchor w3-padding-64 w3-text-red"></i>
+    <img src="https://s3-ap-southeast-1.amazonaws.com/img-in-th/b4c6d636c3bc69243b82e933579fdfb8.png" alt="chingphaow" border="0" />
     </div>
   </div>
 </div>
@@ -156,21 +170,64 @@ class Landingpage extends Component {
     </div>
 
     <div class="w3-twothird">
-      <h1>Lorem Ipsum</h1>
-      <h5 class="w3-padding-32">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</h5>
+      <h1>ข้อควรคำนึงในการชิงเผา</h1>
+      <h5 class="w3-padding-32"> ในการชิงเผา นอกจากจะต้องพิจารณาถึงวิธีการและเทคนิคการเผา
+            ปัจจัยที่กำหนดความรุนแรงของไฟ เครื่องมือและอุปกรณ์ที่ใช้ในการเผา
+            ตลอดจนถึงผลกระทบที่เกิดจากการเผาแล้ว ยังจำเป็นต้อง
+            คำนึงถึงเรื่องต่างๆ ดังต่อไปนี้
+</h5>
 
-      <p class="w3-text-grey">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-        laboris nisi ut aliquip ex ea commodo consequat.</p>
+      <p class="w3-text-grey"> ต้องทำแนวกันไฟรอบพื้นที่ที่จะชิงเผาเสียก่อน
+            เพื่อป้องกันไม่ให้ไฟลุกลามออกไปนอกพื้นที่ชิงเผา
+            โดยอาจอาศัยแนวที่มีอยู่แล้ว เช่น ลำห้วย หรือถนน
+            และต้องมีเจ้าหน้าที่พร้อมเครื่องมือดับไฟป่าคอยควบคุมไม่ให้ไฟลามออกนอกพื้นที่
+            ทำการชิงเผาในช่วงเวลาที่ลมค่อนข้างสงบ อากาศไม่ร้อนจัด
+            และความชื้นสัมพัทธ์ของอากาศค่อนข้างสูง
+            โดยช่วงเวลาที่เหมาะสมสำหรับการชิงเผาอยู่ระหว่าง 2.00 น. ถึง 5.00 น.
+</p>
     </div>
   </div>
 </div>
 
 <div class="w3-container w3-black w3-center w3-opacity w3-padding-64">
-    <h1 class="w3-margin w3-xlarge">Quote of the day: live life</h1>
+    <h1 class="w3-margin w3-xlarge">Chingphaow will save Thailand</h1>
 </div>
+<div class="w3-container w3-light-grey"  id="contact">
+  <br></br><br></br><br></br><br></br>
+<h3 class="w3-center">CONTACT</h3>
+  <p class="w3-center w3-large">Lets get in touch. Send us a message:</p>
+  <div>
+  <p><i class="fa fa-map-marker fa-fw w3-xxlarge w3-margin-right"></i> Ching rai, Thailand</p>
+    <p><i class="fa fa-phone fa-fw w3-xxlarge w3-margin-right"></i> Phone: 0812345</p>
+    <p><i class="fa fa-envelope fa-fw w3-xxlarge w3-margin-right"> </i> Email: mail@mail.com</p>
+    <br></br>
+    <form>
+    <p><input class="w3-input w3-border" type="text" placeholder="Name" required name="Name"></input></p>
+    <p><input class="w3-input w3-border" type="text" placeholder="Email" required name="Email"></input></p>
+    <p><input class="w3-input w3-border" type="text" placeholder="Subject" required name="Subject"></input></p>
+    <p><input class="w3-input w3-border" type="text" placeholder="Message" required name="Message"></input></p>
+    <p>
+        <button class="w3-button w3-black" type="submit">
+          <i class="fa fa-paper-plane"></i> SEND MESSAGE
+        </button>
+      </p>
+    </form>
+  </div>
+
+</div>
+<footer class="w3-center w3-black w3-padding-64">
+  <a href="#homepage" class="w3-button w3-light-grey"><i class="fa fa-arrow-up w3-margin-right"></i>To the top</a>
+  <div class="w3-xlarge w3-section">
+    <i class="fa fa-facebook-official w3-hover-opacity"></i>
+    <i class="fa fa-instagram w3-hover-opacity"></i>
+    <i class="fa fa-snapchat w3-hover-opacity"></i>
+    <i class="fa fa-pinterest-p w3-hover-opacity"></i>
+    <i class="fa fa-twitter w3-hover-opacity"></i>
+    <i class="fa fa-linkedin w3-hover-opacity"></i>
+  </div>
+  <p>Powered by MFUSE</p>
+</footer>
       </div>
-     
 
      
     );
@@ -180,7 +237,7 @@ class Landingpage extends Component {
 }
 const mapStyles = {
     width: '100%',
-    height: '85%',
+    height: '100%',
   };
   
   export default GoogleApiWrapper({
